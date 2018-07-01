cad0c8f51645982ded34abea281c27b85e102bf0
Kingstar <honeysoft@126.com>
2018-07-01 19:03:10 +0800

    init

---
+++ src/main/java/org/bee/hellobee/Hello.java
@@ -0,0 +1,13 @@
+package org.bee.hellobee;
+
+/**
+ * @author KingStar
+ * @since  1.0
+ */
+public class Hello {
+	public static void main(String[] args) {
+		//test again
+		System.out.println("Hello,Bee is ORM framework!");
+	}
+
+}
