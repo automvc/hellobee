03c94e005e890b1bd099dcc5304efd8de7062402
Kingstar <honeysoft@126.com>
2019-02-27 15:04:03 +0800

    test

---
+++ src/main/java/org/automvc/bee/hellobee/Abcd.java
@@ -0,0 +1,26 @@
+package org.automvc.bee.hellobee;
+
+/**
+ * @author Kingstar
+ * @since  1.0
+ */
+//for test
+public class Abcd {
+	private String name;
+	private String addr;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
