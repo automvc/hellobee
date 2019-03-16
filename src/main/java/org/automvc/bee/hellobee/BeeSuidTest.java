1e885d3a521612d2d3440c3a18012f62eeeea62a
Kingstar <honeysoft@126.com>
2019-03-16 11:10:27 +0800

    v1.1.6 add maven info of bee.

---
+++ src/main/java/org/automvc/bee/hellobee/BeeSuidTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(automvc@163.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.automvc.bee.hellobee;
+
+import java.util.List;
+
+import org.teasoft.bee.osql.Suid;
+import org.teasoft.honey.osql.core.BeeFactory;
+
+/**
+ * @author Kingstar
+ * @since  1.0
+ */
+public class BeeSuidTest {
+	public static void main(String[] args) {
+		Suid suid=BeeFactory.getHoneyFactory().getSuid();
+		
+		Orders orders1=new Orders();
+		orders1.setId(100001);
+		orders1.setName("Bee--ORM Framework");
+		
+		List<Orders> list1 =suid.select(orders1);  //select
+		for (int i = 0; i < list1.size(); i++) {
+			System.out.println(list1.get(i).toString());
+		}
+		
+		orders1.setName("Bee--ORM Framework");
+		int updateNum=suid.update(orders1);   //update
+		System.out.println("update record:"+updateNum);
+		
+		Orders orders2=new Orders();
+//		orders2.setUserid("client01");
+		orders2.setName("ORM book");
+//		orders2.setTotal(new BigDecimal(91));
+		orders2.setRemark("");  //empty String test
+		
+		int insertNum=suid.insert(orders2); //insert
+		System.out.println("insert record:"+insertNum);
+		
+		int deleteNum=suid.delete(orders2);   //delete
+		System.out.println("delete record:"+deleteNum);
+		
+		List<Orders> list2 =suid.select(orders1); //select  confirm the data
+		for (int i = 0; i < list2.size(); i++) {
+			System.out.println(list2.get(i).toString());
+		}
+	}
+
+}
