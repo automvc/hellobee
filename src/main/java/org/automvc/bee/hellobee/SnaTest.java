b9c1abe36d5abe13a193388e1172e70ae7739281
Kingstar <honeysoft@126.com>
2019-03-16 11:10:52 +0800

    v1.1.6 add maven info of bee.

---
+++ src/main/java/org/automvc/bee/hellobee/SnaTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(automvc@163.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.automvc.bee.hellobee;
+
+/**
+ * @author Kingstar
+ * @since  1.1.2
+ */
+public class SnaTest {
+	private String name="test";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
