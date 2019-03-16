9d3bdf950655f5698321b08edca3043fcd549c38
Ks <honeysoft@126.com>
2019-03-16 11:05:59 +0800

    v1.1.6 add maven info of bee.

---
+++ src/main/java/org/automvc/bee/hellobee/Orders.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(automvc@163.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.automvc.bee.hellobee;
+
+/**
+ * @author Kingstar
+ * @since  1.1.1
+ */
+public class Orders {
+	private int id;
+	private String name;
+	private String remark;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+}
